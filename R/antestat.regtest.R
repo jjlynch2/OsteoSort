ab1b20870d9441c0cb4ed4c2d92a4e44d55af70b
jjlynch2 <jjlynch2@users.noreply.github.com>
2017-10-10 11:44:20 +1000

    Add files via upload

--- R/antestat.regtest.R
+++ R/antestat.regtest.R
@@ -17,7 +17,7 @@
 antestat.regtest <- function(sort = NULL, ref = NULL, sessiontempdir = NULL, output_options = c(FALSE,FALSE), prediction_interval = 0.95, tails = 2, alphalevel = 0.05, alphatest = TRUE, cores = 1) {
      print("Antemortem stature to postmortem measurement comparisons have started.")	
 	enableJIT(3)
-	
+
 	options(warn = -1) #disables warnings
 	options(as.is = TRUE)
 	if(is.na(ref) || is.null(ref)) {return(NULL)} #input san
@@ -33,10 +33,9 @@ antestat.regtest <- function(sort = NULL, ref = NULL, sessiontempdir = NULL, out
 	nref <- length(measurement) #reference size
 
 	myfunante <- function(X){
-print(X)
-		pm1 <- predict(lm1, newdata = data.frame(measurement = X[6]), interval = "prediction", level = prediction_interval)
+		pm1m <- predict(lm1, newdata = data.frame(measurement = as.numeric(X[6])), interval = "prediction", level = prediction_interval)
 
-		tt <- abs(round(pm1[1], digits=2) - X[2]) / ( summary.lm((lm1))$sigma * sqrt( 1+(1/nref) + ((X[6] - mean(measurement))^2) / (nref * sd(measurement)^2) ) )
+		tt <- abs(round(pm1m[1,1], digits=2) - X[2]) / ( summary.lm((lm1))$sigma * sqrt( 1+(1/nref) + ((X[6] - mean(measurement))^2) / (nref * sd(measurement)^2) ) )
 		tt <- tt[,1] #wtf why is this required Why a data.frame conversion? 
 
 		pp <- tails * pt(-abs(tt), df = nref - 2)
@@ -48,7 +47,7 @@ print(X)
 			else within <- "Excluded"
 		}
 		if(!alphatest) {
-			if(X[2] <= pm1[3] && X[2] >= pm1[2]) { #checks if predicted falls within prediction interval for the predictors
+			if(X[2] <= pm1m[1,3] && X[2] >= pm1m[1,2]) { #checks if predicted falls within prediction interval for the predictors
 				within <- "Cannot Exclude"
 			}
 			else within <- "Excluded"
@@ -58,9 +57,9 @@ print(X)
 			jpeg(paste("graph",X[1],"-", X[3],".jpg",sep=''),height = 400, width = 400)
 			dev.control('enable')
 			plot(stature,measurement, xlab = "Stature", ylab = "Measurement")
-			points(pm1[1],X[6],col="blue",pch=16, cex=1.5)
+			points(pm1m[1,1],X[6],col="blue",pch=16, cex=1.5)
 			points(X[2],X[6],col="red",pch=16, cex=1.5)
-			df1 <- rbind(pm1[1], X[2])
+			df1 <- rbind(pm1m[1,1], X[2])
 			df2 <- rbind(X[6], X[6])
 			matlines(df1, df2, col=c("red"), lty=2)
 			lmp1 <- predict(lm1, interval = "prediction", level = prediction_interval)
@@ -69,33 +68,32 @@ print(X)
 			matlines(lmp1[,3], measurement, col=c("blue"), lty = 4)
 			dev.off()
 		}
-
-		data.frame(X[1],X[2],X[3],X[4],X[5], X[6], round(tt, digits = 2), round(pp, digits=2), nref, round(pm1[2], digits=2), round(pm1[1], digits=2), round(pm1[3], digits=2), round(summary(lm1)$r.squared, digits = 2), within, stringsAsFactors = FALSE)
+		data.frame(X[1],X[2],X[3],X[4],X[5], X[6], round(tt, digits = 2), round(pp, digits=2), nref, round(pm1m[1,2], digits=2), round(pm1m[1,1], digits=2), round(pm1m[1,3], digits=2), round(summary(lm1)$r.squared, digits = 2), within, stringsAsFactors = FALSE)
 	}
 
 	sortlist <- split(sort, 1:nrow(sort))
 
 	if(Sys.info()[['sysname']] == "Windows") {
-		op <- system.time ( hera1 <- lapply(FUN = myfunante, X = sortlist) )
+		op <- system.time ( hera1m <- lapply(FUN = myfunante, X = sortlist) )
 		print(op)
 	}
 	else {
-		op <- system.time ( hera1 <- mclapply(FUN = myfunante, X = sortlist, mc.cores = cores, mc.preschedule = TRUE) )
+		op <- system.time ( hera1m <- mclapply(FUN = myfunante, X = sortlist, mc.cores = cores, mc.preschedule = TRUE) )
 		print(op)
 	}
 
-	hera1 = as.data.frame(data.table::rbindlist(hera1))
-	colnames(hera1) <- c("id","antemortem_stature", "id","side","element","postmortem_measurement", "t-statistic", "p-value","sample_size", "lower_PI","point_estimate","upper_PI", "Rsquared", "Result")
+	hera1m = as.data.frame(data.table::rbindlist(hera1m))
+	colnames(hera1m) <- c("id","antemortem_stature", "id","side","element","postmortem_measurement", "t-statistic", "p-value","sample_size", "lower_PI","point_estimate","upper_PI", "Rsquared", "Result")
 	print("Antemortem stature to postmortem measurement comparisons have completed.")	
 
 	if(output_options[1]) {
 		print("File generation has started.")
-		no_return_value <- OsteoSort:::output_function(hera1)
+		no_return_value <- OsteoSort:::output_function(hera1m)
 		print("File generation has completed.")
 	}
 
 	gc()
 	setwd(workingdir)
 
-	return(list(direc,hera1[hera1$Result == "Cannot Exclude",], hera1[hera1$Result == "Excluded",]))
+	return(list(direc,hera1m[hera1m$Result == "Cannot Exclude",], hera1m[hera1m$Result == "Excluded",]))
 }
