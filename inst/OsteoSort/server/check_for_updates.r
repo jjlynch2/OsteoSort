0ef9d4f7a56a49a099d88f1afe2cb7c837dde128
Jeff Lynch <jjlynch@osteocoder.com>
2018-10-08 17:01:21 +1000

    Removed temp file

--- inst/OsteoSort/server/check_for_updates.r
+++
@@ -1,7 +0,0 @@
-url <- paste0("https://raw.github.com/jjlynch2/OsteoSort/master/DESCRIPTION")
-x <- readLines(url)
-remote_version <- gsub("Version:\\s*", "", x[grep('Version:', x)])
-if(remote_version > packageVersion(gsub(".*/","","OsteoSort"))) {
-
-
-}
