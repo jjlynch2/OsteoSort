6310fd4aaa9d5c267e997be2cd0be777f672ee46
Jeff Lynch <jjlynch@osteocoder.com>
2019-05-31 11:16:05 -0500

    Regression prototype

--- inst/jl/regression_MC.jl
+++ inst/jl/regression_MC.jl
@@ -2,7 +2,8 @@ function REGS(SL, SR, RL, RR)
 	n1 = size(SL,1)
 	n2 = size(SR,1)
 	n3 = size(SL,2)
-	Results = SharedArray{Float64}(n2*n1,n3+7)
+	n4 = size(SR,2)
+	Results = SharedArray{Float64}(n2*n1,n3+n4+7)
 	@sync @distributed for k in 1:n1
 		Results[((k*n2) - n2+1):(k*n2),:] = REGS_worker(SL[k,:], SR, k, RL, RR)
 	end
