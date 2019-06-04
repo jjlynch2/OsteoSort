8caec08487062fd4b81e2d69e056260e52e7c17f
Jeff Lynch <jjlynch@osteocoder.com>
2019-06-04 15:33:59 -0500

    Regression Parallel Update
    Using worker cache due to SharedArrays requiring a bit type, therefore we cannot cache a regression model from GLM in a SharedArray.

--- inst/jl/regression_WC.jl
+++ inst/jl/regression_WC.jl
@@ -57,11 +57,6 @@ end
 	return false
 end
 
-#we can use a sharedarray for caching!!!!!!!!!
-#we can use a sharedarray for caching!!!!!!!!!
-#we can use a sharedarray for caching!!!!!!!!!
-# sharedarray of type tuple?
-
 @everywhere function REGS_worker(v1, m2, li, RL, RR)
 	res = zeros(size(m2,1),size(m2,2)+7+size(v1,1)) #plus 7 and size of measurements for v1 m2?
 	res_1 = measurement_counter(v1) #transposes and counts
@@ -70,53 +65,31 @@ end
 	refd_b = zeros(1,1)
 	m_counter_1_o = 0
 	m_counter_2_o = 0
-
-	m_cache_id = [1,"a"]
-	m_cache_de = [1,"a"]
-	mean_ref_cache = [1,"a"]
-	sd_ref_cache = [1,"a"]
-	n_ref_cache = [1,"a"]
-	r2_ref_cache = [1,"a"]
-	sigma_ref_cache = [1,"a"]
-	OLS_ref_cache = [1,"a"]
+	m_cache_id = []
+	m_cache_de = []
+	mean_ref_cache = []
+	sd_ref_cache = []
+	n_ref_cache = []
+	r2_ref_cache = []
+	sigma_ref_cache = []
+	OLS_ref_cache = []
 	cache_i = false
-
-	pop!(m_cache_id)
-	pop!(m_cache_de)
-	pop!(mean_ref_cache)
-	pop!(sd_ref_cache)
-	pop!(n_ref_cache)
-	pop!(r2_ref_cache)
-	pop!(sigma_ref_cache)
-	pop!(OLS_ref_cache)
-	pop!(m_cache_id)
-	pop!(m_cache_de)
-	pop!(mean_ref_cache)
-	pop!(sd_ref_cache)
-	pop!(n_ref_cache)
-	pop!(r2_ref_cache)
-	pop!(sigma_ref_cache)
-	pop!(OLS_ref_cache)
-
 	res_1_i = 0
 	res_2_i = 0
 	for x in 1:size(m2,1)
 		res_2 = measurement_counter(m2[x,1:end])
 		dsum_2 = log(sum(m2[x,1:end]))
-
 		if x != 1
 			res_1_i = measurement_counter_indices(v1)
 			res_2_i = measurement_counter_indices(m2[x,1:end])
 			cache_i = check_cache(res_1_i, res_2_i, m_cache_id, m_cache_de)
 		end
-
 		refd_1 = zeros(1,1)
 		refd_2 = zeros(1,1)
 		for i in 1:size(RL,1) #works since RL and RR have same number of rows due to R parsing therefore index of row matches specimens
 			if cache_i != false
 				break #break early to pull from cache
 			end
-			
 			m_counter_1 = 0
 			m_counter_2 = 0
 			ref_dsum_1 = 0
@@ -140,7 +113,6 @@ end
 				refd_2 = vcat(refd_2, log(ref_dsum_2))
 			end
 		end
-
 		if cache_i != false
 			mean_ref = mean_ref_cache[cache_i]
 			sd_ref = sd_ref_cache[cache_i]
@@ -158,8 +130,6 @@ end
 			mean_ref = mean(refd_2)
 			sd_ref = std(refd_2)
 			n = size(refd_2,1)
-
-			#save model to cache
 			push!(m_cache_id, res_1_i)
 			push!(m_cache_de, res_2_i)
 			push!(mean_ref_cache, mean_ref)
