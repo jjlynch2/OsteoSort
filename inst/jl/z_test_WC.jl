8faab3a035480df41afb33562a26a2439306e32d
Jeff Lynch <jjlynch@osteocoder.com>
2020-03-12 14:20:40 -0500

    z-tran fix + cache broken

--- inst/jl/z_test_WC.jl
+++ inst/jl/z_test_WC.jl
@@ -1,44 +1,92 @@
 @everywhere function ZTEST_worker(v1, m2, li, RL, RR)
 	res = zeros(size(m2,1),size(m2,2)+7)
+	res_cache = []
+	refd_cache = []
+	refz_cache = []
+	refc_cache = []
+	cache_index = 0
+
+
 	for j in 1:size(m2,1)
+
+###cache isn't working correctly
+###need to test in julia 
+
+
 		#comparison d-values and res for present measurements
 		for g in 1:size(v1,1)
 			if v1[g] != 0 && m2[j,g] != 0 #measurement is being used
 				res[j,g+7] = 1 #still recover index
 			end
 		end
-		refd = ref_dif_s(res[j,8:end], RL, RR)
-		z_temp_ref = zeros(size(refd,1),size(refd,2))
-		c_temp_ref = zeros(size(refd,2))
-		z_temp_comp = zeros(1,size(refd,2))
-		t_temp_comp = zeros(1,size(refd,2))
-		#reference p and t values
-		for i in 1:size(refd,1)
-			for g in 1:size(refd,2)
-				temp = pt(-abs(abs(refd[i,g] - mean(refd[1:end,g])) / std(refd[1:end,g])), size(refd,1) - 1)
-				z_temp_ref[i,g] = qnorm(temp)
-			end
-		end
-		c_temp_ref = cor(z_temp_ref, z_temp_ref)
-		#correlation of z-scores and calculate comparison t, p, z
-		for i in 1:size(refd,2)
-			t_temp_comp[i] = -abs(abs(abs(v1[i] - m2[j,i]) - mean(refd[1:end,i])) / std(refd[1:end,i]))
-			p_temp_comp = pt(t_temp_comp[i], size(refd,1) - 1) #p.value
-			z_temp_comp[i] = qnorm(p_temp_comp)
-		end
-		wA = 0
-		for i in 1:size(t_temp_comp,2)
-			for g in 1:size(t_temp_comp,2)
-				if i != g
-					wA += t_temp_comp[i] * t_temp_comp[g] * c_temp_ref[i,g]
+
+		cache = false
+		temp_index = 0
+		if j != 1
+			for x_cache in 1:cache_index
+				cache = true
+				for i_cache in 1:size(v1,1)
+					if res[j,i_cache] != res_cache[x_cache][i_cache]
+						cache = false
+						break
+					end
+				end
+				if cache
+					temp_index = x_cache
+					break
 				end
 			end
 		end
-		wZ = sum(t_temp_comp .* z_temp_comp) / sqrt(sum(t_temp_comp .^ 2) + (wA * 2)) #combined Z-score
-		wZP = pnorm(-abs(wZ)) #combined P-value from normal distribution
+
+		if cache 
+			refd = refd_cache[temp_index]
+			z_temp_ref = refz_cache[temp_index]
+			c_temp_ref = refc_cache[temp_index]
+		else
+			refd = ref_dif_s(res[j,8:end], RL, RR)
+			z_temp_ref = zeros(size(refd,1),size(refd,2))
+			c_temp_ref = zeros(size(refd,2))
+			
+			#ref z-scores
+			for i in 1:size(refd,1)
+				for g in 1:size(refd,2)
+					z_temp_ref[i,g] = qnorm(2*pt(-abs(abs(refd[i,g] - mean(refd[1:end,g])) / std(refd[1:end,g])), size(refd,1) - 1), true, false)
+				end
+			end
+
+			#ref z-scores correlation
+			c_temp_ref = cor(z_temp_ref, z_temp_ref)
+
+			push!(refc_cache, c_temp_ref)
+			push!(refz_cache, z_temp_ref)
+			push!(refd_cache, refd)
+			push!(res_cache, res)
+			cache_index <- cache_index + 1
+		end
+
+
+		#comparison z-scores
+		z_temp_comp = zeros(1,size(refd,2))
+		for i in 1:size(refd,2)
+			z_temp_comp[i] = qnorm(2*pt(-abs(abs(abs(v1[i] - m2[j,i]) - mean(refd[1:end,i])) / std(refd[1:end,i])), size(refd,1) - 1), true, false)
+		end
+
+		#weights
+		wA = 0
+		wAA = zeros(1,size(refd,2))
+		for i in 1:size(c_temp_ref,2)
+			wAA[i] = mean(refd[1:end,i])/std(refd[1:end,i])
+			for g in i+1:size(c_temp_ref,2)-1
+				wA += wAA[i] * mean(refd[1:end,g])/std(refd[1:end,g]) * c_temp_ref[i,g]
+			end
+		end
+		wZ = sum(wAA .* z_temp_comp) / sqrt(sum(wAA .^ 2) + (wA * 2)) #combined Z-score
+		wZP = 1 - pnorm(wZ, false, false) #combined P-value from normal distribution
+
+
 		res[j,1] = li #index of left
 		res[j,2] = j #index of right
-		res[j,3] = 0 #no needed but left here so array columns match in R
+		res[j,3] = 0 #not needed but left here so array columns match in R
 		res[j,4] = wZP #final P from norm
 		res[j,5] = 0 #since final is norm, return 0 for mean
 		res[j,6] = 1 #since final is norm, return 1 for sd
