9dcd1d108f30e4e4d02bd86ad2de082c5a5fedc4
Jeff Lynch <jjlynch@osteocoder.com>
2020-05-22 22:59:20 -0500

    fix

--- R/digitize.3d.r
+++ R/digitize.3d.r
@@ -16,7 +16,7 @@ digitize.3d <- function(align_data, landmarks = FALSE) {
 		print("Select landmarks")
 		dt <- rep(0,10)
 		ccc <- c("blue","green","red","orange","black","purple","brown","yellow","grey","pink")
-		points3d(align_data, aspect="iso", col = cc)
+		plot3d(align_data, aspect="iso", col = cc, box = TRUE)
 		for(i in as.numeric(landmarks)) {
 			dt[i] <- identify3d(align_data, n = 1)
 			spheres3d(align_data[dt[i],1:3], color = ccc[i])
@@ -24,7 +24,7 @@ digitize.3d <- function(align_data, landmarks = FALSE) {
 		}
 	} else {
 		print("Select fragmented boundary")
-		ids <- plot3d(align_data, aspect = "iso", col=cc)
+		ids <- plot3d(align_data, aspect = "iso", col=cc, box=TRUE)
 		mp <- selectpoints3d(ids["data"], value = FALSE, button = c("right"), multiple = function(ids) {
 				spheres3d(align_data[ids[, "index"], , drop=FALSE], color = "dodgerblue")
 				TRUE
